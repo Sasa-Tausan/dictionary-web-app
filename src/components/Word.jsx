9d762c086bdb36dd6ec1ff3a196e48ed853f888b
Sale1982 <sasatausan@gmail.com>
2023-09-25 14:21:04 +0200

    added logic to display play button based on retur data from api.

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,15 +1,15 @@
-import { useContext, useRef } from 'react';
-import { MyContext } from './MyContext';
-import iconPlay from '../assets/images/icon-play.svg';
+import { useContext, useRef } from "react";
+import { MyContext } from "./MyContext";
+import iconPlay from "../assets/images/icon-play.svg";
 
 const Word = () => {
   const { wordData, theme } = useContext(MyContext);
   let hasAudio = false;
   let audioObject;
-  const { word, phonetics } = wordData[0];
+  const { word, phonetic, phonetics } = wordData[0];
   const audioRef = useRef(null);
   if (phonetics.length !== 0) {
-    audioObject = phonetics.find((item) => item.audio !== '');
+    audioObject = phonetics.find((item) => item.audio !== "");
     hasAudio = !!audioObject;
   }
 
@@ -29,21 +29,28 @@ const Word = () => {
   };
 
   return (
-    <section className='word-section d-flex flex-col '>
-      <div className='d-flex align-items-center space-between'>
-        <div className='d-flex flex-col'>
+    <section className="word-section d-flex flex-col ">
+      <div className="d-flex align-items-center space-between">
+        <div className="d-flex flex-col">
           <h1 className={`heading-l bold text-color-${theme}`}>{word}</h1>
-          <p className='heading-m bold text-color-purple'>{audioObject.text}</p>
+          {hasAudio && audioObject && (
+            <p className="heading-m bold text-color-purple">
+              {audioObject.text}
+            </p>
+          )}
+          {!hasAudio && (
+            <p className="heading-m bold text-color-purple">{phonetic}</p>
+          )}
         </div>
 
         {hasAudio && (
           <button className={`play-btn pointer`} onClick={playAudio}>
             <audio ref={audioRef} src={audioObject.audio}></audio>
-            <img src={iconPlay} alt='' className='play-icon' />
+            <img src={iconPlay} alt="" className="play-icon" />
           </button>
         )}
       </div>
-      <div className='d-flex align-items-center gap-24'>
+      <div className="d-flex align-items-center gap-24">
         <h2 className={`heading-m bold text-color-${theme}`}>noun</h2>
         <hr className={`hr-line full-width bg-hr-${theme}`} />
       </div>
